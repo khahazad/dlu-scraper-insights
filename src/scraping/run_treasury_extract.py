c3244cd432abb84caa6c44c231b3f5314f5368e3
khahazad <khahazad@gmail.com>
2025-12-18 02:25:14 +0100

    Create run_treasury_extract.py

---
+++ src/scraping/run_treasury_extract.py
@@ -0,0 +1,45 @@
+from storage.treasury_csv import load_existing_rows, save_csv
+from scraping.fetch_treasury import extract_page_rows
+
+
+def extract_treasury_log(browser):
+    existing_keys, all_rows = load_existing_rows()
+
+    ctx = browser.new_context(java_script_enabled=False)
+    ctx.route("**/*", lambda route, request: (
+        route.abort()
+        if request.resource_type in ["image", "stylesheet", "font", "script"]
+        else route.continue_()
+    ))
+
+    page = ctx.new_page()
+
+    page_number = 1
+    new_rows = []
+
+    while True:
+        url = f"https://demonicscans.org/guild_treasury_log.php?p={page_number}&res=&kind=donation"
+        page.goto(url, wait_until="domcontentloaded")
+
+        rows = extract_page_rows(page.content())
+        if not rows:
+            break
+
+        stop = False
+        for r in rows:
+            key = (r["Time"], r["PlayerID"], r["Kind"], r["Resource"], r["Amount"])
+            if key in existing_keys:
+                stop = True
+                break
+            new_rows.append(r)
+            existing_keys.add(key)
+
+        if stop:
+            break
+
+        page_number += 1
+
+    ctx.close()
+
+    all_rows = new_rows + all_rows
+    save_csv(all_rows)
