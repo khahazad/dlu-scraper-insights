1445e5b24a437c465e0456778e8fc1488f2bdceb
khahazad <khahazad@gmail.com>
2025-12-19 01:24:57 +0100

    Update run_guild_members_extract.py

--- src/scraping/run_guild_members_extract.py
+++ src/scraping/run_guild_members_extract.py
@@ -1,14 +1,14 @@
 from scraping.fetch_guild_members import extract_guild_members as fetch_members
 from storage.guild_members_csv import merge_members
-
+from utils.protection import assert_page_is_valid
 
 def extract_guild_members(context):
     page = context.new_page()
     page.goto("https://demonicscans.org/guild_members.php", wait_until="domcontentloaded")
-    
+
     # Détection anti-bot
-    assert_page_is_valid(page, "table#guild-members")
-    
+    assert_page_is_valid(page, "table")
+
     members = fetch_members(page)
     merge_members(members)
 
