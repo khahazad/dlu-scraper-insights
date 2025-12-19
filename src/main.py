3e179fe9b1b95af6f67333883660e725e6327370
khahazad <khahazad@gmail.com>
2025-12-19 01:24:10 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,23 +4,26 @@ from scraping.run_guild_members_extract import extract_guild_members
 from scraping.run_player_info_extract import extract_player_info_all
 from scraping.run_treasury_extract import extract_treasury_log
 
-
 def main():
     with sync_playwright() as p:
-        # Login une seule fois
-        browser, context, page = login(p)
+        try:
+            browser, context, page = login(p)
 
-        print("=== Étape 1 : Extraction des membres de la guilde ===")
-        extract_guild_members(context)
+            print("=== Étape 1 : Extraction des membres de la guilde ===")
+            extract_guild_members(context)
 
-        print("=== Étape 2 : Extraction des noms + levels ===")
-        extract_player_info_all(browser)
+            print("=== Étape 2 : Extraction des noms + levels ===")
+            extract_player_info_all(browser)
 
-        print("=== Étape 3 : Extraction des dons ===")
-        extract_treasury_log(browser)
+            print("=== Étape 3 : Extraction des dons ===")
+            extract_treasury_log(browser)
 
-        browser.close()
+        except RuntimeError as e:
+            print(f"ERREUR SCRAPER : {e}")
+            exit(1)
 
+        finally:
+            browser.close()
 
 if __name__ == "__main__":
     main()
