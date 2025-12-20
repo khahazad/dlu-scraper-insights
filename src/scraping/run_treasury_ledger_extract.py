92c44a33cfbc1605e419c4e9a140a5c5523d9948
khahazad <khahazad@gmail.com>
2025-12-20 16:14:50 +0100

    Update and rename run_treasury_extract.py to run_treasury_ledger_extract.py

--- src/scraping/run_treasury_ledger_extract.py
+++ src/scraping/run_treasury_ledger_extract.py
@@ -1,18 +1,8 @@
 from storage.treasury_csv import load_existing_rows, save_csv
-from scraping.fetch_treasury import extract_page_rows
+from scraping.fetch_treasury_ledger import extract_treasury_ledger
 
-
-def extract_treasury_log(browser):
-    existing_keys, all_rows = load_existing_rows()
-
-    ctx = browser.new_context(java_script_enabled=False)
-    ctx.route("**/*", lambda route, request: (
-        route.abort()
-        if request.resource_type in ["image", "stylesheet", "font", "script"]
-        else route.continue_()
-    ))
-
-    page = ctx.new_page()
+def extract_treasury_log(context):
+    page = context.new_page()
 
     page_number = 1
     new_rows = []
@@ -21,7 +11,7 @@ def extract_treasury_log(browser):
         url = f"https://demonicscans.org/guild_treasury_log.php?p={page_number}&res=&kind=donation"
         page.goto(url, wait_until="domcontentloaded")
 
-        rows = extract_page_rows(page.content())
+        rows = extract_treasury_ledger(page)
         if not rows:
             break
 
