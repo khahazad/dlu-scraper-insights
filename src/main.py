3271595191b5afef8af487e2f8ee7d3ad47b8b61
khahazad <khahazad@gmail.com>
2025-12-20 16:55:37 +0100

    Comment out unused extraction functions
    Comment out extraction steps for leaderboard, treasury, and players info.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from playwright.sync_api import sync_playwright
 from auth.login import login
 from scraping.run_guild_members_extract import extract_guild_members
-from scraping.run_weekly_leaderboard_extract import extract_weekly_leaderboard
-from scraping.run_treasury_extract import extract_treasury_ledger
-from scraping.run_players_info_extract import extract_players_info
+#from scraping.run_weekly_leaderboard_extract import extract_weekly_leaderboard
+#from scraping.run_treasury_extract import extract_treasury_ledger
+#from scraping.all_run_players_info_extract import extract_players_info
 
 def main():
     with sync_playwright() as p:
@@ -13,14 +13,14 @@ def main():
             print("=== Step 1 : Guild members extraction ===")
             extract_guild_members(context)
 
-            print("=== Step 2 : Weekly leaderboard extraction ===")
-            extract_weekly_leaderboard(context)
+            #print("=== Step 2 : Weekly leaderboard extraction ===")
+            #extract_weekly_leaderboard(context)
             
-            print("=== Step 3 : Treasury ledger extraction ===")
-            extract_treasury_ledger(context)
+            #print("=== Step 3 : Treasury ledger extraction ===")
+            #extract_treasury_ledger(context)
             
-            print("=== Étape 2 : Players info extraction (name + levels) ===")
-            extract_players_info(context)
+            #print("=== Étape 2 : Players info extraction (name + levels) ===")
+            #extract_players_info(context)
 
         except RuntimeError as e:
             print(f"ERREUR SCRAPER : {e}")
