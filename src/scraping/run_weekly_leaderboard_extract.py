32e98aaaca103ddb3894886c22b853925afa899b
K-Azad <khahazad@gmail.com>
2025-12-21 00:44:32 +0100

    Update run_weekly_leaderboard_extract.py

--- src/scraping/run_weekly_leaderboard_extract.py
+++ src/scraping/run_weekly_leaderboard_extract.py
@@ -1,7 +1,7 @@
 from scraping.fetch_weekly_leaderboard import fetch_weekly_leaderboard
-#from storage.guild_members_csv import merge_members
+#from storage.weekly_leaderboard_csv import merge_weekly_leaderboard
 
-def extract_guild_members(context):
+def extract_weekly_leaderboard(context):
     page = context.new_page()
     url = "https://demonicscans.org/weekly.php"
     page.goto(url, wait_until="domcontentloaded")
