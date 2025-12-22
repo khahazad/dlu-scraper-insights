67cf298ff211c3894b83746cdc0132cab17d9916
K-Azad <khahazad@gmail.com>
2025-12-22 19:40:06 +0100

    Update run_weekly_leaderboard_extract.py

--- src/scraping/run_weekly_leaderboard_extract.py
+++ src/scraping/run_weekly_leaderboard_extract.py
@@ -1,5 +1,5 @@
 from scraping.fetch_weekly_leaderboard import fetch_weekly_leaderboard
-#from storage.weekly_leaderboard_csv import merge_weekly_leaderboard
+from storage.weekly_leaderboard_csv import load_leaderboard_rows, save_csv
 
 def extract_weekly_leaderboard(context):
     page = context.new_page()
@@ -12,3 +12,5 @@ def extract_weekly_leaderboard(context):
     weekly_leaderboard = fetch_weekly_leaderboard(page)
 
     page.close()
+    
+    save_csv(weekly_leaderboard)
