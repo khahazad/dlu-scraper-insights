77bf78e1c2f087b8b26984f7dd0bf0ea37edd00a
K-Azad <khahazad@gmail.com>
2025-12-25 14:01:51 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import sys
 from playwright.sync_api import sync_playwright
 from auth.login import login
+from scraping.scrape_page_table import scrape_first_table
 from scraping.scrape_players_info import scrape_many_players_info
 
 def main():
@@ -27,7 +28,17 @@ def main():
                 67890,
                 # ...
             ]
+            url = "https://demonicscans.org/guild_members.php"
+            guild_members = scrape_first_table(context, url)
+            for gm in guild_members:
+                print(gm)
 
+            page_number = 1
+            url = f"https://demonicscans.org/guild_treasury_log.php?p={page_number}&res=&kind=donation"       
+            treasury_ledger = scrape_first_table(context, url)
+            for tl in treasury_ledger:
+                print(tl)
+                
             # -----------------------------------------
             # 3. Scrape player info (lightweight context)
             # -----------------------------------------
