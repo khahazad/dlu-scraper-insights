d3084be64a233d334f3b8ea2c15137b4d2a542a3
K-Azad <khahazad@gmail.com>
2025-12-25 22:19:19 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -35,10 +35,6 @@ def main():
             print("Scraping treasury ledger info.")
             url_template = "https://demonicscans.org/guild_treasury_log.php?p={page}&res=&kind=donation"
             treasury_ledger = scrape_paginated_tables(context, url_template, 1, "auto")
-            # Clean Rank values: remove leading '#'
-            for pid, fields in weekly_leaderboard.items():
-                if "Rank" in fields and isinstance(fields["Rank"], str):
-                    fields["Rank"] = fields["Rank"].lstrip("#")
             print(f"### dico size {len(treasury_ledger)}")
             for key, data in list(treasury_ledger.items())[:5]:
                 print(key, data)
@@ -47,6 +43,10 @@ def main():
             print("Scraping weekly_leaderboard info.")
             url = "https://demonicscans.org/weekly.php"
             weekly_leaderboard = scrape_first_table(context, url, 1, "pid")
+            # Clean Rank values: remove leading '#'
+            for pid, fields in weekly_leaderboard.items():
+                if "Rank" in fields and isinstance(fields["Rank"], str):
+                    fields["Rank"] = fields["Rank"].lstrip("#")
             print(f"### dico size {len(weekly_leaderboard)}")
             for pid, data in list(weekly_leaderboard.items())[:5]:
                 print(pid, data)
